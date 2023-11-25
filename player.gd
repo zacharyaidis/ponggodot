0a8251129a0341e338ea8881ebaee47aef23133a
Zachary Aidis <zachary.aidis@gmail.com>
2023-11-25 00:27:51 -0500

    Added movement to paddle

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+
+func _physics_process(delta):
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
