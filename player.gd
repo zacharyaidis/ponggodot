31284fdde43f6cdc20b22de7291c5349840d8c10
Zachary Aidis <zachary.aidis@gmail.com>
2023-11-26 12:07:38 -0500

    Small fix for where the player would get moved on the x-axis if it hit the ball at a certain angle. X-coordinate for the player is now locked.

--- player.gd
+++ player.gd
@@ -8,6 +8,9 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	
+	# To ensure that the player paddle's x coordinate does not move
+	position.x = 64
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_up", "ui_down")
@@ -15,5 +18,5 @@ func _physics_process(delta):
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
